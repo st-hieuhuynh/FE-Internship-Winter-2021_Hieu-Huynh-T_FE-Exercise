fd1e3f0d969586e2968166c4492df5b21789bde0
st-hieuhuynh <hieu.huynh@supremetech.vn>
2021-10-31 14:33:49 +0700

    convert cart css to scss and Fix indent

--- js/CartStore.js
+++ js/CartStore.js
@@ -1,16 +1,16 @@
 const _key = "cart";
 
 class CartStore {
-	static readCartStorage = () => {
-		return JSON.parse(window.localStorage.getItem(_key));
-	};
-	static saveToStorage = (cart) => {
-		let cartString = JSON.stringify(cart);
-		window.localStorage.setItem(_key, cartString);
-		let event = document.createEvent("Event");
-		event.initEvent("storage", true, true);
-		window.dispatchEvent(event);
-	};
+  static readCartStorage = () => {
+    return JSON.parse(window.localStorage.getItem(_key));
+  };
+  static saveToStorage = (cart) => {
+    let cartString = JSON.stringify(cart);
+    window.localStorage.setItem(_key, cartString);
+    let event = document.createEvent("Event");
+    event.initEvent("storage", true, true);
+    window.dispatchEvent(event);
+  };
 }
 
 export default CartStore;
--- js/script.js
+++ js/script.js
@@ -3,41 +3,41 @@ import Cart from "./Cart.js";
 import ProductData from "./data.js";
 
 let $productSection = document.querySelector(
-	".selected-product .container .row"
+  ".selected-product .container .row"
 );
 const $main = document.querySelector("main");
 const $footer = document.querySelector("footer");
 // ---- Restyle Header ----
 const $logoImage = document.querySelector("#logo-header");
 const $menuCategoryText = document
-	.querySelector(".menu-category")
-	.querySelectorAll(".text-primary");
+  .querySelector(".menu-category")
+  .querySelectorAll(".text-primary");
 const $searchBtn = document.querySelector("#search-btn");
 const $userBtn = document.querySelector("#user-btn");
 const $cartBtn = document.querySelector("#cart-btn");
 let isMenuLight = true;
 let toggleHeaderMode = () => {
-	if (isMenuLight) {
-		$logoImage.src = "./assets/img/Logo-dark.png";
-		$searchBtn.src = "./assets/img/search-icon-dark.png";
-		$userBtn.src = "./assets/img/user-icon-dark.png";
-		$cartBtn.src = "./assets/img/cart-icon-dark.png";
-	} else {
-		$logoImage.src = "./assets/img/Logo.png";
-		$searchBtn.src = "./assets/img/search-icon.png";
-		$cartBtn.src = "./assets/img/cart-icon.png";
-		$userBtn.src = "./assets/img/user-icon.png";
-	}
-	$menuCategoryText.forEach((ele) => ele.classList.toggle("text-primary"));
-	isMenuLight = !isMenuLight;
+  if (isMenuLight) {
+    $logoImage.src = "./assets/img/Logo-dark.png";
+    $searchBtn.src = "./assets/img/search-icon-dark.png";
+    $userBtn.src = "./assets/img/user-icon-dark.png";
+    $cartBtn.src = "./assets/img/cart-icon-dark.png";
+  } else {
+    $logoImage.src = "./assets/img/Logo.png";
+    $searchBtn.src = "./assets/img/search-icon.png";
+    $cartBtn.src = "./assets/img/cart-icon.png";
+    $userBtn.src = "./assets/img/user-icon.png";
+  }
+  $menuCategoryText.forEach((ele) => ele.classList.toggle("text-primary"));
+  isMenuLight = !isMenuLight;
 };
 // ---- render list product ----
 let list = ProductData.map((element) => {
-	let product = new Product(element);
-	let $li = document.createElement("li");
-	$li.className = "col-3";
-	$li.appendChild(product.render());
-	return $li;
+  let product = new Product(element);
+  let $li = document.createElement("li");
+  $li.className = "col-3 col-lg-3 col-md-6";
+  $li.appendChild(product.render());
+  return $li;
 });
 $productSection.append(...list);
 // ---- cart button ----
@@ -46,10 +46,10 @@ const $cartScreen = document.querySelector(".cart-screen");
 const $goBackBtn = document.querySelector(".go-back");
 $cartBtn.src;
 const toggleCartScreen = () => {
-	$cartScreen.classList.toggle("hide");
-	$main.classList.toggle("hide");
-	$footer.classList.toggle("hide");
-	toggleHeaderMode();
+  $cartScreen.classList.toggle("hide");
+  $main.classList.toggle("hide");
+  $footer.classList.toggle("hide");
+  toggleHeaderMode();
 };
 $goBackBtn.onclick = toggleCartScreen;
 // ---- cart table body handle ----
@@ -61,23 +61,23 @@ const getCartItem = () => Cart.productList.map((item) => Cart.renderItem(item));
 
 // reload cart
 const loadPriceAndQty = () => {
-	$total.innerHTML =
-		"$" +
-		Cart.productList
-			.reduce((prev, cur) => prev + cur.productDetail._price * cur.quantity, 0)
-			.toFixed(2);
-	$quantityBadge.innerHTML = Cart.productList.reduce(
-		(prev, curr) => prev + curr.quantity,
-		0
-	);
+  $total.innerHTML =
+    "$" +
+    Cart.productList
+      .reduce((prev, cur) => prev + cur.productDetail._price * cur.quantity, 0)
+      .toFixed(2);
+  $quantityBadge.innerHTML = Cart.productList.reduce(
+    (prev, curr) => prev + curr.quantity,
+    0
+  );
 };
 window.onload = () => {
-	loadPriceAndQty();
+  loadPriceAndQty();
 };
 $cartBtn.onclick = () => {
-	$cartSection.replaceChildren(...getCartItem());
-	toggleCartScreen();
+  $cartSection.replaceChildren(...getCartItem());
+  toggleCartScreen();
 };
 window.addEventListener("storage", () => {
-	loadPriceAndQty();
+  loadPriceAndQty();
 });
